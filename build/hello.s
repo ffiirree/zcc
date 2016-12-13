58560a9dd9de73b9be0e63a6b6c65c8157e1043d
ffiirree <ice_qi@163.com>
2016-12-13 20:35:35 +0800

    add books.

--- build/hello.s
+++ build/hello.s
@@ -1,13 +1,4 @@
 	.file	"hello.s"
-	.section  .rdata,"dr"
-.LLSTR80:
-	.ascii "\n\0"
-.LLSTR78:
-	.ascii "\n\0"
-.LLSTR73:
-	.ascii "%6d\0"
-.LLSTR65:
-	.ascii "   \0"
 	.def	___main;	.scl	2;	.type	32;	.endef
 	.text
 	.globl	_main
@@ -19,209 +10,47 @@ _main:
 	.cfi_offset 5, -8
 	movl	%esp, %ebp
 	.cfi_def_cfa_register 5
-	subl	$1512, %esp
+	subl	$20, %esp
 	call	___main
-	movl	$18, -16(%ebp)
-	movl	$1, -4(%ebp)
-.Lforb8:
-	movl	-16(%ebp), %eax
-	movl	-4(%ebp), %ebx
-	cmpl	%eax, %ebx
-	jle	.Lfort10
-	jmp	.Lforn7
-.Lfe39:
-	movl	$1, %eax
-	movl	-4(%ebp), %ebx
-	addl	%eax, %ebx
-	movl	%ebx, -4(%ebp)
-	jmp	.Lforb8
-.Lfort10:
+	movl	$250, 0(%esp)
+	movl	$45, 4(%esp)
+	call	_max
+	movl	%eax, -4(%ebp)
 	movl	-4(%ebp), %eax
-	movl	$19, %edx
-	imull	%edx, %eax
-	movl	-4(%ebp), %ebx
-	movl	%eax, %ecx
-	addl	%ebx, %ecx
-	leal	-1460(%ebp), %eax
-	imull	$4, %ecx
-	addl	%ecx, %eax
-	movl	$1, (%eax)
-	movl	-4(%ebp), %eax
-	movl	$19, %edx
-	imull	%edx, %eax
-	movl	-4(%ebp), %ebx
-	movl	%eax, %ecx
-	addl	%ebx, %ecx
-	leal	-1460(%ebp), %eax
-	imull	$4, %ecx
-	addl	%ecx, %eax
-	movl	(%eax), %eax
-	movl	%eax, -1464(%ebp)
-	movl	-4(%ebp), %eax
-	movl	$19, %edx
-	imull	%edx, %eax
-	movl	$1, %ebx
-	movl	%eax, %ecx
-	addl	%ebx, %ecx
-	leal	-1460(%ebp), %eax
-	imull	$4, %ecx
-	addl	%ecx, %eax
-	movl	-1464(%ebp), %ebx
-	movl	%ebx, (%eax)
-	jmp	.Lfe39
-.Lforn7:
-	movl	$3, -4(%ebp)
-.Lforb24:
-	movl	-16(%ebp), %eax
-	movl	-4(%ebp), %ebx
-	cmpl	%eax, %ebx
-	jle	.Lfort26
-	jmp	.Lforn23
-.Lfe325:
-	movl	$1, %eax
-	movl	-4(%ebp), %ebx
-	addl	%eax, %ebx
-	movl	%ebx, -4(%ebp)
-	jmp	.Lforb24
-.Lfort26:
-	movl	$2, -8(%ebp)
-.Lforb31:
-	movl	$1, %eax
-	movl	-4(%ebp), %ebx
-	subl	%eax, %ebx
-	movl	%ebx, %eax
-	movl	-8(%ebp), %ebx
-	cmpl	%eax, %ebx
-	jle	.Lfort33
-	jmp	.Lforn30
-.Lfe332:
-	movl	$1, %eax
-	movl	-8(%ebp), %ebx
-	addl	%eax, %ebx
-	movl	%ebx, -8(%ebp)
-	jmp	.Lforb31
-.Lfort33:
-	movl	$1, %eax
-	movl	-4(%ebp), %ebx
-	subl	%eax, %ebx
-	movl	%ebx, %eax
-	movl	$19, %edx
-	imull	%edx, %eax
-	movl	-8(%ebp), %ebx
-	movl	%eax, %ecx
-	addl	%ebx, %ecx
-	leal	-1460(%ebp), %eax
-	imull	$4, %ecx
-	addl	%ecx, %eax
-	movl	(%eax), %eax
-	movl	%eax, -1468(%ebp)
-	movl	$1, %eax
-	movl	-4(%ebp), %ebx
-	subl	%eax, %ebx
-	movl	%ebx, %eax
-	movl	$19, %edx
-	imull	%edx, %eax
-	movl	$1, %ebx
-	movl	-8(%ebp), %ecx
-	subl	%ebx, %ecx
-	movl	%ecx, %ebx
-	movl	%eax, %ecx
-	addl	%ebx, %ecx
-	leal	-1460(%ebp), %eax
-	imull	$4, %ecx
-	addl	%ecx, %eax
-	movl	(%eax), %eax
-	movl	%eax, -1472(%ebp)
-	movl	-4(%ebp), %eax
-	movl	$19, %edx
-	imull	%edx, %eax
-	movl	-8(%ebp), %ebx
-	movl	%eax, %ecx
-	addl	%ebx, %ecx
-	leal	-1460(%ebp), %eax
-	imull	$4, %ecx
-	addl	%ecx, %eax
-	movl	-1472(%ebp), %ebx
-	movl	-1468(%ebp), %edx
-	addl	%ebx, %edx
-	movl	%edx, (%eax)
-	jmp	.Lfe332
-.Lforn30:
-	jmp	.Lfe325
-.Lforn23:
-	movl	$1, -4(%ebp)
-.Lforb52:
-	movl	-16(%ebp), %eax
-	movl	-4(%ebp), %ebx
-	cmpl	%eax, %ebx
-	jle	.Lfort54
-	jmp	.Lforn51
-.Lfe353:
-	movl	$1, %eax
-	movl	-4(%ebp), %ebx
-	addl	%eax, %ebx
-	movl	%ebx, -4(%ebp)
-	jmp	.Lforb52
-.Lfort54:
-	movl	$1, -12(%ebp)
-.Lforb59:
-	movl	-4(%ebp), %eax
-	movl	-16(%ebp), %ebx
-	subl	%eax, %ebx
-	movl	%ebx, %eax
-	movl	-12(%ebp), %ebx
-	cmpl	%eax, %ebx
-	jle	.Lfort61
-	jmp	.Lforn58
-.Lfe360:
-	movl	$1, %eax
-	movl	-12(%ebp), %ebx
-	addl	%eax, %ebx
-	movl	%ebx, -12(%ebp)
-	jmp	.Lforb59
-.Lfort61:
-	movl	$.LLSTR65, 0(%esp)
-	movl	$1, 4(%esp)
-	call	_printf
-	jmp	.Lfe360
-.Lforn58:
-	movl	$1, -8(%ebp)
-.Lforb68:
-	movl	-4(%ebp), %eax
-	movl	-8(%ebp), %ebx
-	cmpl	%eax, %ebx
-	jle	.Lfort70
-	jmp	.Lforn67
-.Lfe369:
-	movl	$1, %eax
-	movl	-8(%ebp), %ebx
-	addl	%eax, %ebx
-	movl	%ebx, -8(%ebp)
-	jmp	.Lforb68
-.Lfort70:
-	movl	-4(%ebp), %eax
-	movl	$19, %edx
-	imull	%edx, %eax
-	movl	-8(%ebp), %ebx
-	movl	%eax, %ecx
-	addl	%ebx, %ecx
-	leal	-1460(%ebp), %eax
-	imull	$4, %ecx
-	addl	%ecx, %eax
-	movl	(%eax), %eax
-	movl	$.LLSTR73, 0(%esp)
-	movl	%eax, 4(%esp)
-	call	_printf
-	jmp	.Lfe369
-.Lforn67:
-	movl	$.LLSTR78, 0(%esp)
-	movl	$1, 4(%esp)
-	call	_printf
-	jmp	.Lfe353
-.Lforn51:
-	movl	$.LLSTR80, 0(%esp)
-	movl	$1, 4(%esp)
-	call	_printf
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
 	.cfi_endproc
-.LFE82:
+	.text
+	.globl	_max
+	.def	_max;	.scl	2;	.type	32;	.endef
+_max:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	movl	12(%ebp), %eax
+	movl	8(%ebp), %ebx
+	cmpl	%eax, %ebx
+	jg	.Lift8
+	jmp	.Liff9
+.Lift8:
+	movl	8(%ebp), %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	jmp	.Lsn7
+.Liff9:
+	movl	12(%ebp), %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+.Lsn7:
+	.cfi_endproc
+.LFE10:
 	.ident "zcc 0.0.1"
--- simple/hello.c
+++ simple/hello.c
@@ -1,31 +1,14 @@
-#include<stdio.h>
-
-#define  N    19
-
-void main(int argc, char *argv[])
+int max(int a, int b);
+int main()
 {
-    int i, j, k, n = 18, a[N][N];                  /*定义二维数组a[14][14]*/
-    for (i = 1;i <= n;i++) {
-        a[i][i] = 1;
-        int t1 = a[i][i];
-        a[i][1] = t1;
-    }
-    for (i = 3;i <= n;i++) {
-        for (j = 2;j <= i - 1;j++) {
-            int t1 = a[i - 1][j];
-            int t2 = a[i - 1][j - 1];
-            a[i][j] = t1 + t2;                     /*除两边的数外都等于上两顶数之和*/
-        }
-    }
+    int a = max(250, 45);
+    return a;
+}
 
-
-    for (i = 1; i <= n; i++) {
-        for (k = 1; k <= n - i; k++)
-            printf("   ", 1);  
-        for (j = 1; j <= i; j++)
-            printf("%6d", a[i][j]);
-
-        printf("\n", 1);
-    }
-    printf("\n", 1);
+int max(int a, int b)
+{
+    if(a > b)
+        return a;
+    else 
+        return b;
 }
